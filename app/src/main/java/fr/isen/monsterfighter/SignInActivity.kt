3ee89b902e2f960a0eff11d3c39b0c92606eeee5
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2021-02-09 14:31:10 +0100

    Project initialization Internet permission in manifesr and multiple gradle implementations

---
+++ app/src/main/java/fr/isen/monsterfighter/SignInActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.monsterfighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
