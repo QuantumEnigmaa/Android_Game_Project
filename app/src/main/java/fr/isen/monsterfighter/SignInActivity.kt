d8430f88660b8080e4be756cad9264a7b1955f86
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2021-02-09 16:44:58 +0100

    Loading animation added An animation is launched at the start of the app

--- app/src/main/java/fr/isen/monsterfighter/SignInActivity.kt
+++ app/src/main/java/fr/isen/monsterfighter/SignInActivity.kt
@@ -2,10 +2,31 @@ package fr.isen.monsterfighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.monsterfighter.Animations.LoadingAnimation
+import fr.isen.monsterfighter.databinding.ActivitySigninBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySigninBinding
+    private lateinit var loadingAnimation: LoadingAnimation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Loading screen
+        loadingAnimation = LoadingAnimation(this, "loadingScreen.json")
+
+        MainScope().launch {
+            supportActionBar?.hide()
+            loadingAnimation.playAnimation()
+            delay(5500)
+            loadingAnimation.stopAnimation(binding.root)
+            supportActionBar?.show()
+        }
     }
 }
